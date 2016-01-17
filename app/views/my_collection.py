ee89c832a07775af692944d2a29504f6d7503ba0
Kenley Arai <me@kenleyarai.com>
2016-01-16 19:08:04 -0800

    updated my_collection

--- app/views/my_collection.py
+++ app/views/my_collection.py
@@ -5,8 +5,7 @@ my_collection = Blueprint('my_collection', __name__)
 
 @my_collection.route('/my_collection')
 def my_collection_page():
-
-    follows = [x.comics.all() for x in current_user.follows_series]
-    bought = [list(set(current_user.bought_comics) & set(x)) for x in follows]
-    bought = [x for x in bought if x]
+    bought = current_user.bought_comics
+    for i in bought:
+        print i
     return render_template('my_collection.html', subs=bought, login=current_user.connections.full_name)
