365c58a36e12afe649698a85c92e9b6d68a06f4f
Kenley Arai <me@kenleyarai.com>
2016-01-22 18:00:48 -0800

    added procfile runner

--- worker.py
+++ worker.py
@@ -8,7 +8,7 @@ listen = ['default']
 redis_url = os.environ["REDIS_URL"]
 redis_pass = os.environ["REDIS_PASS"]
 
-conn = redis.Redis(url=redis_url, passwprd=redis_pass)
+conn = redis.Redis(url=redis_url, password=redis_pass)
 
 if __name__ == '__main__':
     with Connection(conn):
